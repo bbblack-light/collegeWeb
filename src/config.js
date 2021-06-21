ad5f98bffe99b0eda5c8035b5c91a1950aad87ed
BarakovaES <barakova.2000@mail.ru>
2021-06-21 16:44:05 +0400

    deploy url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ const config = {
   baseUrl: 'http://127.0.0.1:3000/api/data', //ведет на локальный сервер (если использовать в продакшне нужно заменить на url сервера)
   reminder: 'http://127.0.0.1:3000/api/reminder', //url для получения файла Памятки студента
   entrance: 'http://127.0.0.1:3000/api/entrance', //url для получения файла инфы о поступлении
-  springUrl: 'http://127.0.0.1:9191'
+  springUrl: 'https://still-refuge-24182.herokuapp.com' //deploy url
+  // springUrl: 'http://127.0.0.1:9191'
 }
 
 export default config;
